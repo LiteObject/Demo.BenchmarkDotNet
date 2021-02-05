19999ecd88e4004071af7b9f62ff0c54c46b359d
Mohammed <tuniis@live.com>
2021-02-05 15:56:56 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo.BenchmarkDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
